349d39d28a80aff3ae69d5c522cfcc7db27aca1c
Josh Schmidt <oyejorge@gmail.com>
2020-08-14 14:29:08 -0600

    remove autogrow

--- src/selectize.js
+++ src/selectize.js
@@ -194,7 +194,7 @@ $.extend(Selectize.prototype, {
 		$dropdown.on('mouseenter', '[data-selectable]', function() { return self.onOptionHover.apply(self, arguments); });
 		$dropdown.on('mousedown click', '[data-selectable]', function() { return self.onOptionSelect.apply(self, arguments); });
 		watchChildEvent($control, 'mousedown', '*:not(input)', function() { return self.onItemSelect.apply(self, arguments); });
-		autoGrow($control_input);
+		//autoGrow($control_input);
 
 		$control.on({
 			mousedown : function() { return self.onMouseDown.apply(self, arguments); },
