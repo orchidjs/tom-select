372fc7088a8caa6ad5f2ca999f683bc8f6c41ad1
Josh Schmidt <oyejorge@gmail.com>
2020-08-19 23:01:00 -0600

    add vanilla.js for ~jquery functions

--- src/utils.js
+++ src/utils.js
@@ -204,67 +204,6 @@ var getSelection = function(input) {
 };
 
 
-/**
- * Create a new DOM element from an HTML string
- * https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
- *
- * @param {String} html representing a single element
- * @return {Element}
- */
-var htmlToElement = function( html ){
-	
-	if( html.jquery ){
-		return html[0];
-	}
-
-	if( html instanceof HTMLElement ){
-		return html;
-	}
-
-	var div = document.createElement('div');
-	div.innerHTML = html.trim(); // Never return a text node of whitespace as the result
-	return div.firstChild; 
-};
-
-
-/**
- * Return a dom element from either a dom query string, jQuery object or a dom element
- *
- */
-var getDom = function( query ){
-
-	if( query.jquery ){
-		return query[0];
-	}
-
-	if( query instanceof HTMLElement ){
-		return query;
-	}
-
-	return document.querySelector(query);
-};
-
-/**
- * Dispatch an event
- * 
- */
-var triggerEvent = function( dom_el, event_name ){
-	var event = document.createEvent('HTMLEvents');
-	event.initEvent(event_name, true, false);
-	dom_el.dispatchEvent(event)
-};
-
-/**
- * Apply CSS rules to a dom element
- *
- */
-var applyCSS = function( dom_el, css){
-	Object.keys(css).forEach(function(name){
-		dom_el.style[name] = css[name];
-	});
-}
-
-
 var domToString = function(d) {
 	var tmp = document.createElement('div');
 
