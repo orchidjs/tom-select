6140f6b2748f0aaacd4d6d94734fca686c210686
Josh Schmidt <oyejorge@gmail.com>
2020-08-24 17:04:07 -0600

    copy jquery's extend method

--- src/vanilla.js
+++ src/vanilla.js
@@ -61,57 +61,111 @@ var applyCSS = function( dom_el, css){
 
 
 /**
- * Merge the contents of two or more objects together into the first object.
+ * Copied from jQuery source
  *
  */
-var deepAssign = function(obj1){
-	var extended = {};
-	var i;
+var hasOwn 		= ( {} ).hasOwnProperty;
+var fnToString	= hasOwn.toString;
 
-	if( Array.isArray(obj1) ){
-		extended = [];
+var isPlainObject = function( obj ) {
+	var proto, Ctor;
+
+	// Detect obvious negatives
+	// Use toString instead of jQuery.type to catch host objects
+	if ( !obj || toString.call( obj ) !== "[object Object]" ) {
+		return false;
 	}
 
+	proto = getProto( obj );
 
-	// Merge the object into the extended object
-	// If deep merge and property is an object, merge properties
-	var merge = function (obj) {
+	// Objects with no prototype (e.g., `Object.create( null )`) are plain
+	if ( !proto ) {
+		return true;
+	}
 
-		var prop;
-		for ( prop in obj ) {
+	// Objects with prototype are plain iff they were constructed by a global Object function
+	Ctor = hasOwn.call( proto, "constructor" ) && proto.constructor;
+	return typeof Ctor === "function" && fnToString.call( Ctor ) === ObjectFunctionString;
+};
 
-			if( !obj.hasOwnProperty(prop) ){
-				continue;
-			}
 
-			if( obj[prop] === null ){ 
-				continue;
-			}
+var isFunction = function( obj ) {
 
-			// create array
-			if( !(prop in extended) && Array.isArray(obj[prop]) ){
-				extended[prop] = [];
-			}
+	// Support: Chrome <=57, Firefox <=52
+	// In some browsers, typeof returns "function" for HTML <object> elements
+	// (i.e., `typeof document.createElement( "object" ) === "function"`).
+	// We don't want to classify *any* DOM node as a function.
+	return typeof obj === "function" && typeof obj.nodeType !== "number";
+};
 
-			// deep copy if object
-			if( typeof(obj[prop]) === 'object' ){
-				if( obj[prop].jquery ){
-					extended[prop] = obj[prop].clone();
-				}else{
-					extended[prop] = deepAssign( extended[prop], obj[prop] );
+var extend = function() {
+	var options, name, src, copy, copyIsArray, clone,
+		target = arguments[ 0 ] || {},
+		i = 1,
+		length = arguments.length,
+		deep = false;
+
+	// Handle a deep copy situation
+	if ( typeof target === "boolean" ) {
+		deep = target;
+
+		// Skip the boolean and the target
+		target = arguments[ i ] || {};
+		i++;
+	}
+
+	// Handle case when target is a string or something (possible in deep copy)
+	if ( typeof target !== "object" && !isFunction( target ) ) {
+		target = {};
+	}
+
+	// Extend jQuery itself if only one argument is passed
+	if ( i === length ) {
+		target = this;
+		i--;
+	}
+
+	for ( ; i < length; i++ ) {
+
+		// Only deal with non-null/undefined values
+		if ( ( options = arguments[ i ] ) != null ) {
+
+			// Extend the base object
+			for ( name in options ) {
+				copy = options[ name ];
+
+				// Prevent Object.prototype pollution
+				// Prevent never-ending loop
+				if ( name === "__proto__" || target === copy ) {
+					continue;
 				}
-			}else{
-				extended[prop] = obj[prop];
+
+				// Recurse if we're merging plain objects or arrays
+				if ( deep && copy && ( jQuery.isPlainObject( copy ) ||
+					( copyIsArray = Array.isArray( copy ) ) ) ) {
+					src = target[ name ];
+
+					// Ensure proper type for the source value
+					if ( copyIsArray && !Array.isArray( src ) ) {
+						clone = [];
+					} else if ( !copyIsArray && !jQuery.isPlainObject( src ) ) {
+						clone = {};
+					} else {
+						clone = src;
+					}
+					copyIsArray = false;
+
+					// Never move original objects, clone them
+					target[ name ] = jqExtend( deep, clone, copy );
+
+				// Don't bring in undefined values
+				} else if ( copy !== undefined ) {
+					target[ name ] = copy;
+				}
 			}
-
 		}
-	};
-
-	// Loop through each object and conduct a merge
-	for( i = 0; i < arguments.length; i++){
-		merge(arguments[i]);
 	}
 
-	return extended;
-
+	// Return the modified object
+	return target;
 };
