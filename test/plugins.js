c1f5453aff02acb21b25a1145f56d3f372e81b9c
Josh Schmidt <oyejorge@gmail.com>
2020-09-01 20:12:30 -0600

    add placeholder for plugin tests

---
+++ test/plugins.js
@@ -0,0 +1,11 @@
+(function() {
+
+	describe('plugins', function() {
+
+		it_n('plugins ', function() {
+			//let test = setup_test('<input type="text">', {plugins: ['input_autogrow']});
+		});
+
+	});
+
+})();
