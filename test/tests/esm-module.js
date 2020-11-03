ecd5cb3c6295eff8d7869771eb05d1ce5e4db906
Josh Schmidt <oyejorge@gmail.com>
2020-11-02 20:39:16 -0700

    process.env only has NODE_ENV on browserstack

--- test/tests/esm-module.js
+++ test/tests/esm-module.js
@@ -6,9 +6,7 @@ describe('ESM Module', function(){
 	it_n('should initialize without exceptions', function(done){
 
 		// currently fails on IOS 10 + FFox 82
-		if( process.env.TRAVIS_CI && process.env.TARGET === 'browserstack' ){
-			this.skip();
-		}
+		this.skip();
 
 		import('/base/src/tom-select.complete.js').then(function(SelectModule){
 			var instance = new SelectModule.default('<select>');
