60d9ff4cb709ead295e5d6f64cd6a0864acc6c55
Josh Schmidt <oyejorge@gmail.com>
2021-03-09 08:21:19 -0700

    add tests for checkbox options

---
+++ test/tests/plugins/checkbox_options.js
@@ -0,0 +1,84 @@
+
+
+
+describe('plugin: checkbox_options', function() {
+
+	it_n('active items should be checked on load', function(done) {
+
+		let test = setup_test('<input value="a,b">', {plugins: ['checkbox_options']});
+		syn.click(test.instance.control_input,function(){
+			var checked = test.instance.dropdown.querySelectorAll('input:checked');
+			assert.equal(checked.length,2);
+			done();
+		});
+
+	});
+
+	it_n('checkbox should be updated after option is clicked', function(done) {
+
+		let test = setup_test('AB_Multi', {plugins: ['checkbox_options']});
+		syn.click(test.instance.control_input,function(){
+
+			var option = test.instance.getOption('a');
+			var checkbox = option.querySelector('input');
+
+			// check/active
+			syn.click(option,function(){
+				assert.deepEqual(test.instance.items, ['a']);
+				assert.equal(checkbox.checked, true,'checkbox not checked');
+
+				// uncheck
+				syn.click(option,function(){
+					assert.deepEqual(test.instance.items, []);
+					assert.equal(checkbox.checked, false,'checkbox checked');
+					done();
+
+				});
+
+			});
+
+		});
+
+	});
+
+	it_n('checkbox should be checked after checkbox is clicked', function(done) {
+
+		let test = setup_test('AB_Multi', {plugins: ['checkbox_options']});
+		syn.click(test.instance.control_input,function(){
+
+			var option = test.instance.getOption('a');
+			var checkbox = option.querySelector('input');
+
+			// check/active
+			syn.click(checkbox,function(){
+				assert.deepEqual(test.instance.items, ['a']);
+				assert.equal(checkbox.checked, true,'checkbox not checked');
+
+				// uncheck
+				syn.click(checkbox,function(){
+					assert.deepEqual(test.instance.items, []);
+					assert.equal(checkbox.checked, false,'checkbox checked');
+					done();
+
+				});
+			});
+		});
+	});
+
+
+	it_n('removing item before dropdown open should not check option', function(done) {
+
+		let test = setup_test('<input value="a">', {plugins: ['checkbox_options']});
+		test.instance.removeItem('a');
+
+		syn.click(test.instance.control_input,function(){
+			var option = test.instance.getOption('a');
+			var checkbox = option.querySelector('input');
+			assert.equal(checkbox.checked, false,'checkbox checked');
+			done();
+		});
+
+	});
+
+
+});
