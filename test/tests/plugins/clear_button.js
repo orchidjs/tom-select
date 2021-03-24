33c10f397c3fd0d7bba3cd396b3fcd58a0f87117
Josh Schmidt <oyejorge@gmail.com>
2021-03-23 23:16:44 -0600

    trigger just one onchange event for clear_button. fixes #46 (test added)

--- test/tests/plugins/clear_button.js
+++ test/tests/plugins/clear_button.js
@@ -5,16 +5,25 @@ describe('plugin: clear_button', function() {
 
 	it_n('should remove all item when button is clicked', function(done) {
 
-		let test = setup_test('AB_Multi', {plugins: ['clear_button']});
+		var on_change_calls = 0;
+		let test = setup_test('AB_Multi', {
+			plugins: ['clear_button'],
+			onChange:function(){
+				on_change_calls++;
+			}
+		});
 
 		test.instance.addItem('a');
 		test.instance.addItem('b');
+
+		on_change_calls = 0;
 		assert.equal( test.instance.items.length, 2 );
 
 		var button			= test.instance.control.querySelector('.clear-button');
 
 		syn.click( button, function() {
-			assert.equal( test.instance.items.length, 0 );
+			assert.equal( test.instance.items.length, 0, 'should clear items array'  );
+			assert.equal( on_change_calls, 1,'should only call onChange once' );
 			done();
 		});
 
