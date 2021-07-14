f9f83c26ee6a5446ac85bcd99d65319fdb5c50cc
Josh Schmidt <oyejorge@gmail.com>
2021-07-14 17:18:57 -0600

    tests: keyboard interaction for dropdown-input

--- test/tests/plugins/dropdown_input.js
+++ test/tests/plugins/dropdown_input.js
@@ -6,11 +6,7 @@ describe('plugin: dropdown_input', function() {
 	it_n('dropdown should open onclick', function(done) {
 		let test = setup_test('<input value="a,b" tabindex="1" placeholder="test placeholder" />', {plugins: ['dropdown_input']});
 
-		// confirm controlInput is in dropdown
-		// assert.equal( test.instance.dropdown.contains(test.instance.settings.controlInput), true);
-
-		// confirm placeholder has been applied to dropdown input
-		// assert.equal( test.instance.settings.controlInput.getAttribute('placeholder'), 'test placeholder');
+		assert.isTrue( test.instance.dropdown.contains(test.instance.control_input), 'control_input should be in dropdown');
 
 		syn.click(test.instance.control).delay(0,function(){
 			assert.equal(test.instance.isOpen, true);
@@ -27,17 +23,28 @@ describe('plugin: dropdown_input', function() {
 
 	});
 
-	it_n('[enter] on wrapper should open', function(done) {
+	it_n('should select option with [enter] keypress (single)', async () => {
 
-		let test = setup_test('<input value="a,b" tabindex="1" />', {plugins: ['dropdown_input']});
+		var test = setup_test('AB_Single');
 
-		assert.equal(test.instance.focus_node.tabIndex, 1);
+		await asyncClick(test.instance.control);
+		
+		assert.equal(test.instance.activeOption.dataset.value,'a');
 
-		syn.type('[enter]',test.instance.wrapper,function(){
-			assert.equal(test.instance.isOpen, true);
-			done();
-		});
+		await asyncType('a', test.instance.control_input);
+		await asyncType('[enter]', test.instance.control_input);
+		
+		assert.equal( test.instance.items.length, 1);
+		assert.equal( test.instance.items[0], 'a');
+		assert.equal( test.instance.control_input.value, '', 'control_input.value != ""' );
 
+		await asyncType('[b]', test.instance.control_input);
+		await asyncType('[enter]', test.instance.control_input);
+
+		assert.equal( test.instance.items.length, 1);
+		assert.equal( test.instance.items[0], 'b');
+		assert.equal( test.instance.control_input.value, '', 'control_input.value != ""' );
+	
 	});
 
 });
