a33b221b0600c4b01fcf96409cac51cbb0dd1a33
Josh Schmidt <oyejorge@gmail.com>
2022-05-18 21:52:03 -0600

    use local settings variable instead of self.settings

--- src/tom-select.ts
+++ src/tom-select.ts
@@ -274,13 +274,13 @@ export default class TomSelect extends MicroPlugin(MicroEvent){
 			setAttr(input,{multiple:'multiple'});
 		}
 
-		if (self.settings.placeholder) {
+		if (settings.placeholder) {
 			setAttr(control_input,{placeholder:settings.placeholder});
 		}
 
 		// if splitOn was not passed in, construct it from the delimiter to allow pasting universally
-		if (!self.settings.splitOn && self.settings.delimiter) {
-			self.settings.splitOn = new RegExp('\\s*' + escape_regex(self.settings.delimiter) + '+\\s*');
+		if (!settings.splitOn && settings.delimiter) {
+			settings.splitOn = new RegExp('\\s*' + escape_regex(settings.delimiter) + '+\\s*');
 		}
 
 		// debounce user defined load() if loadThrottle > 0
