d26d3a00833364b25f73d71637baa8ee4aead271
oyejorge <oyejorge@users.noreply.github.com>
2022-06-30 14:26:04 +0000

    deploy: 33106d4f72f58ee3d5e537fa91fe862174efa390

--- js/tom-select.popular.js
+++ js/tom-select.popular.js
@@ -2115,7 +2115,7 @@
 	          preventDefault(e); // if the option_create=null, the dropdown might be closed
 	        } else if (self.settings.create && self.createItem()) {
 	          preventDefault(e); // don't submit form when searching for a value
-	        } else if (document.activeElement == self.control_input && this.isOpen) {
+	        } else if (document.activeElement == self.control_input && self.isOpen) {
 	          preventDefault(e);
 	        }
 
