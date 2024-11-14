73f3295119e6f73805f0dca1eae7d94efdc925ff
Srdjan Radosavljevic <62420807+matrix2305@users.noreply.github.com>
2024-11-14 22:33:56 +0100

    Fix octal escape sequences (#777)
    * Fix octal escape sequences
    
    * Delete .idea/.gitignore
    
    * Update src/vanilla.ts
    
    ---------
    
    Co-authored-by: Nathan Sarang-Walters <nwalters512@gmail.com>

--- src/vanilla.ts
+++ src/vanilla.ts
@@ -97,7 +97,7 @@ export const classesArray = (args:string[]|string[][]):string[] => {
 	var classes:string[] = [];
 	iterate( args, (_classes) =>{
 		if( typeof _classes === 'string' ){
-			_classes = _classes.trim().split(/[\11\12\14\15\40]/);
+			_classes = _classes.trim().split(/[\t\n\f\r\s]/);
 		}
 		if( Array.isArray(_classes) ){
 			classes = classes.concat(_classes);
