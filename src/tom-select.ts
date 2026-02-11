6cb39c472462bad49015dcc2944de058b86e1382
Hannes <31671206+xJuvi@users.noreply.github.com>
2026-02-11 13:00:33 +0100

    change logic to avoid type script errors

--- src/tom-select.ts
+++ src/tom-select.ts
@@ -1365,10 +1365,9 @@ export default class TomSelect extends MicroPlugin(MicroEvent){
 			// temp fix for https://github.com/orchidjs/tom-select/issues/987
 			// UI crashed when more than 30 same chars in a row, prevent search and return empt result
 			if (/(.)\1{15,}/.test(query)) {
-				result 				= { items: [], total: 0 } as PrepareObj;
-			} else {
-				result 				= self.sifter.search(query, Object.assign(options, { score: calculateScore }));
+				query 				= '';
 			}
+			result 					= self.sifter.search(query, Object.assign(options, { score: calculateScore }));
 			self.currentResults		= result;
 		} else {
 			result					= Object.assign( {}, self.currentResults);
